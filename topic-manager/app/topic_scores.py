6df9dc82456aec5262dc0336940f6a126d7d264a
Donald Cameron <donald.cameron@qa.com>
2025-06-23 19:42:00 +0100

    Added v2.1

--- topic-manager/app/topic_scores.py
+++ topic-manager/app/topic_scores.py
@@ -1,2 +1,11 @@
 class TopicScores:
-    pass
+    def __init__(self, topic_name, scores):
+        self._topic_name = topic_name
+        self._scores = scores
+
+    def get_topic_name(self):
+        return self._topic_name
+
+    def get_scores(self):
+        return self._scores
+
